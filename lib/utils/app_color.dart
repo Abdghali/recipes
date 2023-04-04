0c392e83ad35d5c9919bd4273d1f1cc05acd9d02
Abdghali <abdghali13@gmail.com>
2023-04-04 14:51:13 +0200

    add app_color

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// defines a class named AppColor with static constant properties for green, orange, and brown colors, using the hexadecimal ARGB values.
+/// You can use these constants in your Flutter app by importing the file that defines the AppColor class, and then accessing the color properties like AppColor.green, AppColor.orange, and AppColor.brown.
+class AppColor {
+  static const Color green = Color(0xFF228B22);
+  static const Color orange = Color(0xFFFF8C00);
+  static const Color brown = Color(0xFF8B4513);
+}

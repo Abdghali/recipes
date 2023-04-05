f99ed6d370336461a5f62b37c5555b23980e122e
Abdghali <abdghali13@gmail.com>
2023-04-05 15:21:44 +0200

    create app bar helper

---
+++ lib/models/app_bar_helper.dart
@@ -0,0 +1,11 @@
+class AppBarHelper {
+  static String _title = 'Recipes';
+
+  static String getTitle() {
+    return _title;
+  }
+
+  static void setTitle(String newTitle) {
+    _title = newTitle;
+  }
+}
